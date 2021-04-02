158a479548f18db5e6576185a6fbd713a1c2b41f
Adam Kostandy <adamkostandy@gmail.com>
2021-04-02 07:47:23 -0400

    Ending and Returning
    Deleted all WIKI versions and Added index for fun.

--- Docuwiki/lib/plugins/vshare/lang/cs/lang.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- *
- * @author Jakub Duchek <jakduch@jakduch.cz>
- */
-$lang['js']['prompt']          = 'Vložte prosím celý odkaz na stránku videa zde:';
