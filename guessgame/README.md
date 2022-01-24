777ec19275f4a355c1bde5882619bf63411aa425
diabeatz96 <75499553+diabeatz96@users.noreply.github.com>
2022-01-24 13:46:08 -0500

    PUSHING PROJECT 2
    PROJECT 2 PUSH

---
+++ guessgame/README.md
@@ -0,0 +1,32 @@
+<h1 align="center">Welcome to Guessing Game 1.0 👋</h1>
+<p>
+  <img alt="Version" src="https://img.shields.io/badge/version-1.0-blue.svg?cacheSeconds=2592000" />
+  <a href="https://github.com/diabeatz96/GuessingGame" target="_blank">
+    <img alt="Documentation" src="https://img.shields.io/badge/documentation-yes-brightgreen.svg" />
+  </a>
+  <a href="https://twitter.com/bigblobgames" target="_blank">
+    <img alt="Twitter: bigblobgames" src="https://img.shields.io/twitter/follow/bigblobgames.svg?style=social" />
+  </a>
+</p>
+
+> This game is a simple guessing simulator where the user has to write in a number, and if that number is correct, they win!
+
+## Author
+
+👤 **Adam and Esteban**
+
+* Website: https://thegoldencutproductions.com/
+* Twitter: [@bigblobgames](https://twitter.com/bigblobgames)
+* Github: [@diabeatz96](https://github.com/diabeatz96)
+* LinkedIn: [@adamkostandy](https://linkedin.com/in/adamkostandy)
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!<br />Feel free to check [issues page](https://github.com/diabeatz96/GuessingGame/issues). 
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+***
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
---
+++ guessgame/js/guess.js
@@ -0,0 +1,107 @@
+// Number guessing
+
+let y = Math.floor(Math.random() * 100 + 1);
+let guess = 1;
+let score = 10;
+const historyArray = [];
+const backgroundArray = ["images/bg1.gif", "images/bg2.jpg", "images/b3.jpg", "images/b4.jpg", "images/2825711.gif"];
+let currentBackground = 0;
+let progressBar = document.querySelector(".progress-bar");
+
+document.querySelector("#score").innerHTML = `Type a number to get started`;
+
+
+/**
+ *
+ * UTILITY FUNCTIONS
+ *
+ * **/
+
+
+
+/** Function for clicking on reset **/
+document.querySelector("#reset").onclick = function() {
+   historyArray.splice(0,historyArray.length);
+   y = Math.floor(Math.random() * 100 + 1);
+   score = 10;
+
+   document.querySelector("#score").innerHTML = `Reset! Try again your life total is back to: ${score}`;
+   document.querySelector(".card-text").innerHTML = "";
+
+   progressBar.ariaValueNow = "0";
+   progressBar.style.width = "0%";
+};
+
+/** Fun utility function for changing background back and forth. */
+
+document.querySelector("#changebg").onclick = function() {
+      if(currentBackground === 4) {
+         currentBackground = 0;
+         document.querySelector("body").style.backgroundImage = `url('${backgroundArray[currentBackground]}')`;
+      }
+         document.querySelector("body").style.backgroundImage = `url('${backgroundArray[currentBackground]}')`;
+         currentBackground++;
+}
+
+
+
+/*** MAIN CALL BACK FOR CLICK ON BUTTON
+ *   MAIN SECTION PROCESS SCORE ETC.
+ */
+document.querySelector("#submit_guess").onclick = function()
+{
+
+   let x = document.querySelector("#guess_field").value;
+   // Point system
+   // starting score 
+
+   if( x < 0 || x > 100) {
+      document.querySelector("#score").innerHTML = `Please pick a number between 0 and 100`;
+      return;
+   }
+
+   if (score == 0)
+   {
+      document.querySelector("#score").innerHTML = `You are at 0 health points! Click reset to try again`;
+      return;
+   }
+
+   if(x == y)
+      {
+         document.querySelector('#score').innerHTML = "Great! You guessed the number right in " + guess + " guess! Your health is " + score + " points" ;
+         progressBar.ariaValueNow = "100";
+         progressBar.style.width = "100%";
+      }
+   else if(x > y)
+      {
+         guess++;
+         score -= 1;
+         historyArray.push(x);
+         document.querySelector("#score").innerHTML = `Not a winner! Choose a smaller number! health: ${score}`;
+         document.querySelector(".card-text").innerHTML += "<li>" + historyArray[historyArray.length - 1] + "</li>" ;
+      }
+   else if (x < y)
+      {
+         guess++;
+         score -= 1;
+         historyArray.push(x);
+         document.querySelector("#score").innerHTML = `Not a winner! Choose a greater number! health: ${score}`;
+         document.querySelector(".card-text").innerHTML += "<li>" + historyArray[historyArray.length - 1] + "</li>" ;
+      }
+
+   /** Determining progress bar width **/
+
+   if(Math.abs(x - y) > 50) {
+      progressBar.ariaValueNow = "25";
+      progressBar.style.width = "25%";
+   } else if(Math.abs(x - y) > 25) {
+      progressBar.ariaValueNow = "50";
+      progressBar.style.width = "50%";
+   } else if(Math.abs(x - y) >= 2) {
+      progressBar.ariaValueNow = "75";
+      progressBar.style.width = "75%";
+   }
+}
+
+
+
